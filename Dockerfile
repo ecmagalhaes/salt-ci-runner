e2cbb70e151886de93abec28fe6d0af84515344b
Stefan Monko <smonko@simianlabs.io>
2018-05-23 15:39:58 +0200

    set master to auto accept minions
    Signed-off-by: Stefan Monko <smonko@simianlabs.io>

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ ADD run_salt.sh /root/run_salt.sh
 
 RUN apt-get update && apt-get install -y curl wget && \
     curl -L https://bootstrap.saltstack.com -o install_salt.sh && \
-    sh install_salt.sh -P -M && chmod a+x /root/run_salt.sh
+    sh install_salt.sh -P -M && \
+    echo "open_mode: True" > /etc/salt/master.d/master.conf && \
+    echo "auto_accept: True" >> /etc/salt/master.d/master.conf && \
+    chmod a+x /root/run_salt.sh
 
 CMD ["/root/run_salt.sh"]
--- run_salt.sh
+++ run_salt.sh
@@ -29,25 +29,25 @@ if [ $status -ne 0 ]; then
   echo "Failed to start salt-master: $status"
   exit $status
 fi
-for (( c=1; c<=30; c++ ))
-do
-   salt-key -A -y
-done
-# Naive check runs checks once a minute to see if either of the processes exited.
-# This illustrates part of the heavy lifting you need to do if you want to run
-# more than one service in a container. The container will exit with an error
-# if it detects that either of the processes has exited.
-# Otherwise it will loop forever, waking up every 60 seconds
-while /bin/true; do
-  ps aux |grep salt-master |grep -q -v grep
-  PROCESS_1_STATUS=$?
-  ps aux |grep salt-minion |grep -q -v grep
-  PROCESS_2_STATUS=$?
-  # If the greps above find anything, they will exit with 0 status
-  # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
-    echo "One of the processes has already exited."
-    exit -1
-  fi
-  sleep 60
-done
+# for (( c=1; c<=30; c++ ))
+# do
+#    salt-key -A -y
+# done
+# # Naive check runs checks once a minute to see if either of the processes exited.
+# # This illustrates part of the heavy lifting you need to do if you want to run
+# # more than one service in a container. The container will exit with an error
+# # if it detects that either of the processes has exited.
+# # Otherwise it will loop forever, waking up every 60 seconds
+# while /bin/true; do
+#   ps aux |grep salt-master |grep -q -v grep
+#   PROCESS_1_STATUS=$?
+#   ps aux |grep salt-minion |grep -q -v grep
+#   PROCESS_2_STATUS=$?
+#   # If the greps above find anything, they will exit with 0 status
+#   # If they are not both 0, then something is wrong
+#   if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
+#     echo "One of the processes has already exited."
+#     exit -1
+#   fi
+#   sleep 60
+# done
